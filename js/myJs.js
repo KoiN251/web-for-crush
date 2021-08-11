8b8ce1d51b74b4b8a7ddcbe24af1ca6405d657a7
KoiN251 <88781061+KoiN251@users.noreply.github.com>
2021-08-11 20:16:25 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "chào vy!",
+  text2: "T có điều này muốn hỏi m nhớ phải trả lời thật lòng nhaaa.",
+  text3: "m thích t có phải không ha?._.",
+  text4: "Nếu mà ko trả lời rồi thoát ra  là muốn quen t đó nha :v",
+  text5: "M mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do m thích t đi :vvvv",
+  text8: "Gửi cho t <3",
+  text9: "Vì m đẹp trai lại hát hay vlllll",
+  text10: "T biết mà ^^ Yêu m 3k",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    " Đọc xong rồi ha. Còn giờ thì chờ gì nữa mà ko inbox cho t đi ",
   text12: "Okii lunn <3",
 };
 
